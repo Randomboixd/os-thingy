2c27c2a700c946717494895c92105bc803cfac8b
Randomboixd <100033803+Randomboixd@users.noreply.github.com>
2024-11-09 21:12:01 +0100

    Rename to McMojave-circle and hope that it finally applies normally

--- files/scripts/fetch_mcmojave_icons.sh
+++ files/scripts/fetch_mcmojave_icons.sh
@@ -12,12 +12,12 @@ echo "Unzipping mcmojave_icons.zip"
 unzip -qq mcmojave_icons.zip
 
 
-echo "Copying mcmojave_icons/src to usr/share/icons/McMojave"
-cp -r McMojave-circle-2024-04-11/src/ /usr/share/icons/McMojave
+echo "Copying mcmojave_icons/src to usr/share/icons/McMojave-circle"
+cp  McMojave-circle-2024-04-11/src /usr/share/icons/McMojave-circle
 # I think we also need to copy that COPYING file which is the gpl-3.0 license. I really don't know what i am doing... But yeah we copy it over... just in case.
-cp McMojave-circle-2024-04-11/COPYING /usr/share/icons/McMojave/COPYING
+cp McMojave-circle-2024-04-11/COPYING /usr/share/icons/McMojave-circle/COPYING
 # And also the authors.
-cp McMojave-circle-2024-04-11/AUTHORS /usr/share/icons/McMojave/AUTHORS
+cp McMojave-circle-2024-04-11/AUTHORS /usr/share/icons/McMojave-circle/AUTHORS
 
 # Finally clean up and remove McMojave-circle-2024-04-11 and it's zip.
 rm -r ./McMojave-circle-2024-04-11
