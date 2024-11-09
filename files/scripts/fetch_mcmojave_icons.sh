469af61006ae387790e0b99d8cd63567c34dc3bc
Randomboixd <100033803+Randomboixd@users.noreply.github.com>
2024-11-09 19:14:22 +0100

    pretty please?

--- files/scripts/fetch_mcmojave_icons.sh
+++ files/scripts/fetch_mcmojave_icons.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
+echo "Current path: $(pwd)"
+
 echo "Attempting to download McMojave-circle icons..."
 echo "To be honest i am not entirely sure if this will work but here we go."
 
@@ -9,14 +11,15 @@ curl -L -o mcmojave_icons.zip https://github.com/vinceliuice/McMojave-circle/arc
 echo "Unzipping mcmojave_icons.zip"
 unzip mcmojave_icons.zip
 
+
 echo "Copying mcmojave_icons/src to files/usr/share/icons/McMojave"
 mkdir -p files/usr/share/icons/McMojave
-cp -r mcmojave_icons/src files/usr/share/icons/McMojave
+cp -r McMojave-circle-2024-04-11/src files/usr/share/icons/McMojave
 # I think we also need to copy that COPYING file which is the gpl-3.0 license. I really don't know what i am doing... But yeah we copy it over... just in case.
-cp mcmojave_icons/COPYING files/usr/share/icons/McMojave/COPYING
+cp McMojave-circle-2024-04-11/COPYING files/usr/share/icons/McMojave/COPYING
 # And also the authors.
-cp mcmojave_icons/AUTHORS files/usr/share/icons/McMojave/AUTHORS
+cp McMojave-circle-2024-04-11/AUTHORS files/usr/share/icons/McMojave/AUTHORS
 
-# Finally clean up and remove mcmojave_icons and it's zip.
-rm -r ./mcmojave_icons
+# Finally clean up and remove McMojave-circle-2024-04-11 and it's zip.
+rm -r ./McMojave-circle-2024-04-11
 rm mcmojave_icons.zip
