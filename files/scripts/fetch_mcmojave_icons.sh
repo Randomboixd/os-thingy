84143aadc480b46c3495c7671e83409b5cf31010
Randomboixd <100033803+Randomboixd@users.noreply.github.com>
2024-11-09 19:36:22 +0100

    wait. OH RIGHT files has a subfolder called system MEANING WE JUST COPIED EVERYTHING TO A FOLDER THAT ISN'T EVEN IN THE SYSTEM wow. let's fix that.

--- files/scripts/fetch_mcmojave_icons.sh
+++ files/scripts/fetch_mcmojave_icons.sh
@@ -9,16 +9,16 @@ echo "To be honest i am not entirely sure if this will work but here we go."
 curl -L -o mcmojave_icons.zip https://github.com/vinceliuice/McMojave-circle/archive/refs/tags/2024-04-11.zip
 
 echo "Unzipping mcmojave_icons.zip"
-unzip mcmojave_icons.zip
+unzip -qq mcmojave_icons.zip
 
 
-echo "Copying mcmojave_icons/src to files/usr/share/icons/McMojave"
-mkdir -p files/usr/share/icons/McMojave
-cp -r McMojave-circle-2024-04-11/src files/usr/share/icons/McMojave
+echo "Copying mcmojave_icons/src to files/system/usr/share/icons/McMojave"
+mkdir -p files/system/usr/share/icons/McMojave
+cp -r McMojave-circle-2024-04-11/src files/system/usr/share/icons/McMojave
 # I think we also need to copy that COPYING file which is the gpl-3.0 license. I really don't know what i am doing... But yeah we copy it over... just in case.
-cp McMojave-circle-2024-04-11/COPYING files/usr/share/icons/McMojave/COPYING
+cp McMojave-circle-2024-04-11/COPYING files/system/usr/share/icons/McMojave/COPYING
 # And also the authors.
-cp McMojave-circle-2024-04-11/AUTHORS files/usr/share/icons/McMojave/AUTHORS
+cp McMojave-circle-2024-04-11/AUTHORS files/system/usr/share/icons/McMojave/AUTHORS
 
 # Finally clean up and remove McMojave-circle-2024-04-11 and it's zip.
 rm -r ./McMojave-circle-2024-04-11
