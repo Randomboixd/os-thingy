2e42b2b8f5f24de7a66c8f19e5b2646e3de2d7c0
Randomboixd <100033803+Randomboixd@users.noreply.github.com>
2024-11-09 18:59:59 +0100

    I like my GNOME themed bruh

---
+++ files/scripts/fetch_mcmojave_icons.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+echo "Attempting to download McMojave-circle icons..."
+echo "To be honest i am not entirely sure if this will work but here we go."
+
+curl -L -o mcmojave_icons.zip https://github.com/vinceliuice/McMojave-circle/archive/refs/tags/2024-04-11.zip
+
+echo "Unzipping mcmojave_icons.zip"
+unzip mcmojave_icons.zip
+
+echo "Copying mcmojave_icons/src to usr/share/icons/McMojave"
+mkdir -p usr/share/icons/McMojave
+cp -r mcmojave_icons/src usr/share/icons/McMojave
+# I think we also need to copy that COPYING file which is the gpl-3.0 license. I really don't know what i am doing... But yeah we copy it over... just in case.
+cp mcmojave_icons/COPYING usr/share/icons/McMojave/COPYING
+# And also the authors.
+cp mcmojave_icons/AUTHORS usr/share/icons/McMojave/AUTHORS
+
+# Finally clean up and remove mcmojave_icons and it's zip.
+rm -r ./mcmojave_icons
+rm mcmojave_icons.zip
